44e49dda476a1dee691f8df4f5b8e3dc0195deaf
ilyxabatko <ilyxabatko@inbox.ru>
2023-03-06 21:08:24 +0400

    adds new_order & cancel_order  ixs

--- Cluster2/challenge2/deposit/programs/deposit/src/lib.rs
+++ Cluster2/challenge2/deposit/programs/deposit/src/lib.rs
@@ -1,90 +1,381 @@
-use anchor_lang::{
-    prelude::*,
-    system_program::{self, Transfer}
+use anchor_lang::{prelude::*, system_program};
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    dex::{
+        self, cancel_order_v2,
+        serum_dex::{
+            instruction::SelfTradeBehavior,
+            matching::{OrderType, Side},
+        },
+        CancelOrderV2, Dex, NewOrderV3,
+    },
+    token::{Mint, Token, TokenAccount, Transfer as SplTransfer},
 };
+use std::num::NonZeroU64;
 
 declare_id!("GWZPQsbLKGnB8rQptKGDFsZ3PtbsYPFxhXm8NyhoftbY");
 
+// Got the implementation from Richard, rewrote everything to get familiar with the proper way
+// of initializing, sending and withdrawing tokens
 #[program]
 pub mod deposit {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        let vault_account = &mut ctx.accounts.vault;
-
-        vault_account.initializer_key = *ctx.accounts.initializer.key;
-
+        let deposit_account = &mut ctx.accounts.deposit_account;
+        deposit_account.deposit_auth = *ctx.accounts.deposit_auth.key;
+        ctx.accounts.deposit_account.auth_bump = *ctx.bumps.get("pda_auth").unwrap();
         Ok(())
     }
 
     pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
-        system_program::transfer(ctx.accounts.into_deposit_vault_context(), amount)?;
+        let deposit_account = &mut ctx.accounts.deposit_account;
+        let deposit_auth = &ctx.accounts.deposit_auth;
+        let system_program = &ctx.accounts.system_program;
+
+        deposit_account.sol_vault_bump = ctx.bumps.get("sol_vault").copied();
+
+        let cpi_accounts = system_program::Transfer {
+            from: deposit_auth.to_account_info(),
+            to: ctx.accounts.sol_vault.to_account_info(),
+        };
+
+        let cpi = CpiContext::new(system_program.to_account_info(), cpi_accounts);
+
+        system_program::transfer(cpi, amount)?;
+
+        Ok(())
+    }
+
+    pub fn deposit_spl(ctx: Context<DepositSpl>, amount: u64) -> Result<()> {
+        let cpi_accounts = SplTransfer {
+            from: ctx.accounts.from_token_acct.to_account_info(),
+            to: ctx.accounts.to_token_acct.to_account_info(),
+            authority: ctx.accounts.deposit_auth.to_account_info(),
+        };
+
+        let cpi = CpiContext::new(ctx.accounts.token_program.to_account_info(), cpi_accounts);
+
+        anchor_spl::token::transfer(cpi, amount)?;
 
         Ok(())
     }
 
     pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
-        let receiver = &mut ctx.accounts.initializer;
-        let vault = &mut ctx.accounts.vault;
+        let system_program = &ctx.accounts.system_program;
+        let deposit_account = &ctx.accounts.deposit_account;
+        let pda_auth = &mut ctx.accounts.pda_auth;
+        let sol_vault = &mut ctx.accounts.sol_vault;
 
-        **vault.to_account_info().try_borrow_mut_lamports()? -= amount;
-        **receiver.try_borrow_mut_lamports()? += amount;
+        let cpi_accounts = system_program::Transfer {
+            from: sol_vault.to_account_info(),
+            to: ctx.accounts.deposit_auth.to_account_info(),
+        };
+
+        let seeds = &[
+            b"sol_vault",
+            pda_auth.to_account_info().key.as_ref(),
+            &[deposit_account.sol_vault_bump.unwrap()],
+        ];
+        let signer = &[&seeds[..]];
+
+        let cpi =
+            CpiContext::new_with_signer(system_program.to_account_info(), cpi_accounts, signer);
+
+        system_program::transfer(cpi, amount)?;
 
         Ok(())
     }
 
-    
+    pub fn withdraw_spl(ctx: Context<WithdrawSpl>, amount: u64) -> Result<()> {
+        let deposit_account = &ctx.accounts.deposit_account;
+
+        let cpi_accounts = SplTransfer {
+            from: ctx.accounts.from_token_acct.to_account_info(),
+            to: ctx.accounts.to_token_acct.to_account_info(),
+            authority: ctx.accounts.pda_auth.to_account_info(),
+        };
+
+        let seeds = &[
+            b"auth",
+            deposit_account.to_account_info().key.as_ref(),
+            &[deposit_account.auth_bump],
+        ];
+
+        let signer = &[&seeds[..]];
+
+        let cpi = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            cpi_accounts,
+            signer,
+        );
+
+        anchor_spl::token::transfer(cpi, amount)?;
+
+        Ok(())
+    }
+
+    pub fn new_order(ctx: Context<NewOrder>, limit_price: NonZeroU64) -> Result<()> {
+        let dex_program = ctx.accounts.dex_program.to_account_info();
+
+        let side: Side = Side::Ask;
+        let max_coin_qty = NonZeroU64::new(1000000000000000000).unwrap();
+        let max_native_pc_qty_including_fees = NonZeroU64::new(1000000000000000000).unwrap();
+        let self_trade_behavior = SelfTradeBehavior::DecrementTake;
+        let order_type = OrderType::Limit;
+        let client_order_id = 0;
+        let limit = 100u16;
+
+        // I didn't manage to complete the coding challenge with the following line:
+        // let accounts: NewOrderV3<'static> = ctx;
+        // because ctx.accounts.into() was trying to consume the "ctx.accounts" (take ownership)
+        // and it's impossible since we have a mutable accounts references
+        // so I manually created the struct
+
+        let accounts: NewOrderV3 = NewOrderV3 {
+            market: ctx.accounts.market.to_account_info(),
+            open_orders: ctx.accounts.open_orders.to_account_info(),
+            request_queue: ctx.accounts.request_queue.to_account_info(),
+            event_queue: ctx.accounts.event_queue.to_account_info(),
+            market_bids: ctx.accounts.market_bids.to_account_info(),
+            market_asks: ctx.accounts.market_asks.to_account_info(),
+            order_payer_token_account: ctx.accounts.order_payer_token_account.to_account_info(),
+            open_orders_authority: ctx.accounts.open_orders_authority.to_account_info(),
+            coin_vault: ctx.accounts.coin_vault.to_account_info(),
+            pc_vault: ctx.accounts.pc_vault.to_account_info(),
+            token_program: ctx.accounts.token_program.to_account_info(),
+            rent: ctx.accounts.rent.to_account_info(),
+        };
+
+        let cpi = CpiContext::new(dex_program, accounts.into());
+
+        dex::new_order_v3(
+            cpi,
+            side,
+            limit_price,
+            max_coin_qty,
+            max_native_pc_qty_including_fees,
+            self_trade_behavior,
+            order_type,
+            client_order_id,
+            limit,
+        )?;
+
+        Ok(())
+    }
+
+    pub fn cancel_order(ctx: Context<CancelOrder>) -> Result<()> {
+        let side: Side = Side::Ask;
+        let order_id: u128 = 1;
+
+        let cpi_accounts = CancelOrderV2 {
+            market: ctx.accounts.market.to_account_info(),
+            market_bids: ctx.accounts.market_bids.to_account_info(),
+            market_asks: ctx.accounts.market_asks.to_account_info(),
+            open_orders: ctx.accounts.open_orders.to_account_info(),
+            open_orders_authority: ctx.accounts.open_orders_authority.to_account_info(),
+            event_queue: ctx.accounts.event_queue.to_account_info(),
+        };
+
+        let cpi = CpiContext::new(ctx.accounts.dex_program.to_account_info(), cpi_accounts);
+        cancel_order_v2(cpi, side, order_id)
+    }
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
+    #[account(init_if_needed, payer = deposit_auth, space = Vault::SIZE)]
+    pub deposit_account: Account<'info, Vault>,
+    #[account(seeds = [b"auth", deposit_account.key().as_ref()], bump)]
+    /// CHECK: no need to check it
+    pub pda_auth: UncheckedAccount<'info>,
     #[account(mut)]
-    pub initializer: Signer<'info>,
-    #[account(
-        init_if_needed, payer = initializer, space = Vault::SIZE, 
-        seeds=[b"vault", initializer.key().as_ref()], 
-        bump
-    )]
-    pub vault: Account<'info, Vault>,
-    #[account(address = system_program::ID)]
-    pub system_program: Program<'info, System>
+    pub deposit_auth: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
 pub struct Deposit<'info> {
+    #[account(mut, has_one = deposit_auth)]
+    pub deposit_account: Account<'info, Vault>,
+    #[account(seeds = [b"auth", deposit_account.key().as_ref()], bump = deposit_account.auth_bump)]
+    /// CHECK: no need to check it
+    pub pda_auth: UncheckedAccount<'info>,
+    #[account(mut, seeds = [b"sol_vault", pda_auth.key().as_ref()], bump)]
+    pub sol_vault: SystemAccount<'info>,
     #[account(mut)]
-    pub initializer: Signer<'info>,
-    #[account(mut)]
-    pub vault: Account<'info, Vault>,
-    pub system_program: Program<'info, System>
+    pub deposit_auth: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
 pub struct Withdraw<'info> {
+    #[account(has_one = deposit_auth)]
+    pub deposit_account: Account<'info, Vault>,
+    #[account(seeds = [b"auth", deposit_account.key().as_ref()], bump = deposit_account.auth_bump)]
+    /// CHECK: no need to check it
+    pub pda_auth: UncheckedAccount<'info>,
+    #[account(mut, seeds = [b"sol_vault", pda_auth.key().as_ref()], bump = deposit_account.sol_vault_bump.unwrap())]
+    pub sol_vault: SystemAccount<'info>,
     #[account(mut)]
-    pub initializer: AccountInfo<'info>,
+    pub deposit_auth: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct DepositSpl<'info> {
+    #[account(has_one = deposit_auth)]
+    pub deposit_account: Account<'info, Vault>,
+    #[account(seeds = [b"auth", deposit_account.key().as_ref()], bump = deposit_account.auth_bump)]
+    /// CHECK: no need to check it
+    pub pda_auth: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub deposit_auth: Signer<'info>,
     #[account(
-        mut,
-        constraint = vault.initializer_key == initializer.key()
+        init_if_needed,
+        associated_token::mint = token_mint,
+        payer = deposit_auth,
+        associated_token::authority = pda_auth,
     )]
-    pub vault: Account<'info, Vault>
+    pub to_token_acct: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub from_token_acct: Account<'info, TokenAccount>,
+    pub token_mint: Account<'info, Mint>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct WithdrawSpl<'info> {
+    #[account(has_one = deposit_auth)]
+    pub deposit_account: Account<'info, Vault>,
+    #[account(seeds = [b"auth", deposit_account.key().as_ref()], bump = deposit_account.auth_bump)]
+    /// CHECK: no need to check it
+    pub pda_auth: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub deposit_auth: Signer<'info>,
+    #[account(mut)]
+    pub to_token_acct: Account<'info, TokenAccount>,
+    #[account(mut,
+        associated_token::mint = token_mint,
+        associated_token::authority = pda_auth,
+    )]
+    pub from_token_acct: Account<'info, TokenAccount>,
+    pub token_mint: Account<'info, Mint>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct CreateLimit<'info> {
+    #[account(has_one = deposit_auth)]
+    pub deposit_account: Account<'info, Vault>,
+    #[account(seeds = [b"auth", deposit_account.key().as_ref()], bump = deposit_account.auth_bump)]
+    /// CHECK: no need to check
+    pub pda_auth: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub deposit_auth: Signer<'info>,
+    #[account(
+        init, 
+        seeds = [b"limit", token_mint.key().as_ref(), deposit_account.key().as_ref()], 
+        bump, 
+        payer = deposit_auth, 
+        space = Limit::LEN
+    )]
+    pub limit_account: Account<'info, Limit>,
+    pub token_account: Account<'info, TokenAccount>,
+    pub token_mint: Account<'info, Mint>,
+    #[account(owner = Token::id())]
+    pub ask_token_mint: Account<'info, Mint>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct NewOrder<'info> {
+    /// CHECK: no need to check
+    pub market: AccountInfo<'info>,
+    /// CHECK: no need to check
+    pub open_orders: AccountInfo<'info>,
+    /// CHECK: no need to check
+    pub request_queue: AccountInfo<'info>,
+    /// CHECK: no need to check
+    pub event_queue: AccountInfo<'info>,
+    /// CHECK: no need to check
+    pub market_bids: AccountInfo<'info>,
+    /// CHECK: no need to check
+    pub market_asks: AccountInfo<'info>,
+    // Token account where funds are transferred from for the order. If
+    // posting a bid market A/B, then this is the SPL token account for B.
+    /// CHECK: no need to check
+    pub order_payer_token_account: AccountInfo<'info>,
+    /// CHECK: no need to check
+    pub open_orders_authority: AccountInfo<'info>,
+    // Also known as the "base" currency. For a given A/B market,
+    // this is the vault for the A mint.
+    /// CHECK: no need to check
+    pub coin_vault: AccountInfo<'info>,
+    // Also known as the "quote" currency. For a given A/B market,
+    // this is the vault for the B mint.
+    /// CHECK: no need to check
+    pub pc_vault: AccountInfo<'info>,
+    /// CHECK: no need to check
+    pub token_program: AccountInfo<'info>,
+    pub dex_program: Program<'info, Dex>,
+    /// CHECK: no need to check
+    pub rent: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct CancelOrder<'info> {
+    /// CHECK: no need to check
+    pub market: AccountInfo<'info>,
+    /// CHECK: no need to check
+    pub market_bids: AccountInfo<'info>,
+    /// CHECK: no need to check
+    pub market_asks: AccountInfo<'info>,
+    /// CHECK: no need to check
+    pub open_orders: AccountInfo<'info>,
+    /// CHECK: no need to check
+    pub open_orders_authority: AccountInfo<'info>,
+    /// CHECK: no need to check
+    pub event_queue: AccountInfo<'info>,
+    pub dex_program: Program<'info, Dex>,
 }
 
 #[account]
 pub struct Vault {
-    pub initializer_key: Pubkey,
+    pub deposit_auth: Pubkey,
+    pub auth_bump: u8,
+    pub sol_vault_bump: Option<u8>,
+}
+
+#[account]
+pub struct Limit {
+    pub asset_holding_pda: Option<Pubkey>,
+    pub asset: Asset,
+    pub ask_price_per_asset: u64,
+    pub ask_asset: Asset,
+    pub ask_asset_pda: Option<Pubkey>,
+}
+
+#[account]
+pub struct Asset {
+    pub asset_type: String,
+    pub asset_metadata: Option<Pubkey>,
+    pub asset_mint: Option<Pubkey>,
 }
 
 impl Vault {
-    const SIZE: usize = 80; // descriminator (8 bytes), 2 pubkeys (2 * 32 bytes) & u64 (8 bytes)
+    const SIZE: usize = 43; // descriminator (8 bytes), 1 pubkey (32 bytes), u8 (1 byte) & Option<T> (1 + 1 bytes)
 }
 
-impl<'info> Deposit<'info> {
-    fn into_deposit_vault_context(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
-        let cpi_accounts = Transfer {
-            from: self.initializer.to_account_info().clone(),
-            to: self.vault.to_account_info().clone()
-        };
-        CpiContext::new(self.system_program.to_account_info(), cpi_accounts)
-    }
+impl Limit {
+    const LEN: usize = 33 + Asset::LEN + 8 + Asset::LEN + 33; // Option = 32 + 1  (2 times), Asset::LEN (2 times),
+                                                              // u64 = 8 bytes
+}
+
+impl Asset {
+    const LEN: usize = 32 + 33 + 33; // String = 32, 2 Option enums = (1 + 32) * 2
 }
--- Cluster2/challenge2/deposit/tests/deposit.ts
+++ Cluster2/challenge2/deposit/tests/deposit.ts
@@ -1,51 +1,240 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { Deposit } from "../target/types/deposit";
-import * as web3 from "@solana/web3.js"
+import { ASSOCIATED_TOKEN_PROGRAM_ID, createMint, getAssociatedTokenAddressSync, getMint, getOrCreateAssociatedTokenAccount, mintToChecked, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { execSync } from "child_process";
+import { keypairIdentity, Metaplex, toBigNumber } from "@metaplex-foundation/js";
 
-describe("deposit program", () => {
-    const anchorProvider = anchor.AnchorProvider.env();
-    anchor.setProvider(anchorProvider);
+describe("deposit", () => {
+    let provider = anchor.AnchorProvider.local("http://127.0.0.1:8899");
     const program = anchor.workspace.Deposit as Program<Deposit>;
+    const deposit_account = anchor.web3.Keypair.generate();
+    const deposit_auth = anchor.web3.Keypair.generate();
+    const metaplex = Metaplex.make(provider.connection).use(keypairIdentity(deposit_auth));
+    let mint = anchor.web3.Keypair.generate();
+    let usdc_auth = anchor.web3.Keypair.generate();
 
-    const initializer_pubkey = anchorProvider.wallet.publicKey;
-    const [vault_pda, _] = web3.PublicKey.findProgramAddressSync([Buffer.from("vault", "binary"), initializer_pubkey.toBuffer()], program.programId);
+    const TOKEN_METADATA_PROGRAM_ID = anchor.web3.Keypair.generate();
 
-    const LAMPORTS_PER_SOL = anchor.web3.LAMPORTS_PER_SOL;
-    const amount_to_deposit: anchor.BN = new anchor.BN(0.2 * anchor.web3.LAMPORTS_PER_SOL);
-    const amount_to_withdraw: anchor.BN = new anchor.BN(0.1 * anchor.web3.LAMPORTS_PER_SOL);
+    let [pda_auth, pda_bump] = anchor.web3.PublicKey.findProgramAddressSync(
+        [
+            anchor.utils.bytes.utf8.encode("auth"),
+            deposit_account.publicKey.toBuffer()
+        ],
+        program.programId
+    );
 
-    it("Inits a vault", async () => {
+    let [sol_vault, sol_bump] = anchor.web3.PublicKey.findProgramAddressSync(
+        [
+            anchor.utils.bytes.utf8.encode("sol_vault"),
+            pda_auth.toBuffer()
+        ],
+        program.programId
+    );
+
+    // execSync(
+    //     `anchor idl init --filepath target/idl/deposit.json ${program.programId}`,
+    //     { stdio: "inherit" }
+    // );
+
+    before(async () => {
+        let latestBlockHash = await provider.connection.getLatestBlockhash();
+
+        let res = await provider.connection.requestAirdrop(
+            deposit_auth.publicKey,
+            100 * anchor.web3.LAMPORTS_PER_SOL
+        );
+
+        await provider.connection.confirmTransaction({
+            blockhash: latestBlockHash.blockhash,
+            lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+            signature: res,
+        });
+
+        console.log("Deposit_auth balance: ", await provider.connection.getBalance(deposit_auth.publicKey));
+        
+    });
+
+    it("Is initialized!", async () => {
         const tx = await program.methods.initialize()
             .accounts({
-                initializer: anchorProvider.wallet.publicKey,
-                vault: vault_pda,
-                systemProgram: web3.SystemProgram.programId
-            })
-            .rpc();
-        console.log("Transaction signature", tx);
+                depositAccount: deposit_account.publicKey,
+                pdaAuth: pda_auth,
+                depositAuth: deposit_auth.publicKey,
+                systemProgram: anchor.web3.SystemProgram.programId,
+            }).signers([deposit_account, deposit_auth]).rpc();
+
+        console.log("Your transaction signature", tx);
+
+        let result = await program.account.vault.fetch(deposit_account.publicKey);
+        console.log(result);
     });
 
-    it("Deposits to vault", async () => {
-        const tx_send = await program.methods.deposit(amount_to_deposit)
+    it("Deposits native SOL", async () => {
+        const deposit_amount = new anchor.BN(25 * anchor.web3.LAMPORTS_PER_SOL);
+        const deposit_native_tx = await program.methods.deposit(deposit_amount)
             .accounts({
-                initializer: initializer_pubkey,
-                vault: vault_pda,
-                systemProgram: web3.SystemProgram.programId
-            })
-            .rpc();
-        console.log(`Deposited ${amount_to_deposit.toNumber() / LAMPORTS_PER_SOL} SOL from ${initializer_pubkey} to the vault: ${vault_pda}.`);
-        console.log("Transaction signature", tx_send);
+                depositAccount: deposit_account.publicKey,
+                pdaAuth: pda_auth,
+                solVault: sol_vault,
+                depositAuth: deposit_auth.publicKey,
+                systemProgram: anchor.web3.SystemProgram.programId,
+            }).signers([deposit_auth]).rpc();
+
+        let sol_vault_lamps = await provider.connection.getBalance(sol_vault);
+        console.log(sol_vault_lamps);
+
+        let result = await program.account.vault.fetch(deposit_account.publicKey);
+        console.log(result);
     });
 
-    it("Withdraws from vault", async () => {
-        const tx_send = await program.methods.withdraw(amount_to_withdraw)
+    it("Withdraws native SOL", async () => {
+        let withdraw_amount = new anchor.BN(1 * anchor.web3.LAMPORTS_PER_SOL);
+
+        const withdraw_native_tx = await program.methods.withdraw(withdraw_amount)
             .accounts({
-                initializer: initializer_pubkey,
-                vault: vault_pda
-            })
-            .rpc();
-        console.log(`Withdrawal ${amount_to_withdraw.toNumber() / LAMPORTS_PER_SOL} SOL from vault to ${initializer_pubkey}.`);
-        console.log("Transaction signature", tx_send);
-      });
+                depositAccount: deposit_account.publicKey,
+                pdaAuth: pda_auth,
+                solVault: sol_vault,
+                depositAuth: deposit_auth.publicKey,
+                systemProgram: anchor.web3.SystemProgram.programId,
+            }).signers([deposit_auth]).rpc();
+
+        let sol_vault_lamps = await provider.connection.getBalance(sol_vault);
+        console.log(sol_vault_lamps);
+    });
+
+    it("Create mock SPL Token", async () => {
+
+        let token_mint = await createMint(
+            provider.connection,
+            deposit_auth,
+            usdc_auth.publicKey,
+            usdc_auth.publicKey,
+            6,
+            mint,
+            null,
+            TOKEN_PROGRAM_ID
+        );
+
+        let test = await getMint(provider.connection, token_mint, null, TOKEN_PROGRAM_ID);
+        console.log(test);
+
+        let deposit_auth_usdc_acct = await getOrCreateAssociatedTokenAccount(provider.connection, deposit_auth, token_mint, deposit_auth.publicKey, false, undefined, undefined, TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID)
+
+        let mint_to_sig = await mintToChecked(provider.connection, deposit_auth, token_mint, deposit_auth_usdc_acct.address, usdc_auth, 200e6, 6, [], undefined, TOKEN_PROGRAM_ID);
+
+        console.log(mint_to_sig);
+
+    });
+
+    it("Deposits SPL Token", async () => {
+        let to_token_acct = getAssociatedTokenAddressSync(mint.publicKey, pda_auth, true, TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID);
+
+        let from_token_acct = getOrCreateAssociatedTokenAccount(provider.connection, deposit_account, mint.publicKey, deposit_auth.publicKey, false);
+        let from_token_acct_wallet = (await from_token_acct).address;
+
+
+        let deposit_spl_tx = await program.methods.depositSpl(new anchor.BN(25e6)).accounts({
+            depositAccount: deposit_account.publicKey,
+            pdaAuth: pda_auth,
+            depositAuth: deposit_auth.publicKey,
+            toTokenAcct: to_token_acct,
+            fromTokenAcct: from_token_acct_wallet,
+            tokenMint: mint.publicKey,
+            tokenProgram: TOKEN_PROGRAM_ID,
+            associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+            systemProgram: anchor.web3.SystemProgram.programId,
+        }).signers([deposit_auth]).rpc();
+
+        console.log(deposit_spl_tx);
+
+    });
+
+    // not sure about the implementation, hence I don't run it for now
+    xit("Withdraws SPL Token", async () => {
+        let to_token_acct = getAssociatedTokenAddressSync(mint.publicKey, pda_auth, true, TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID);
+
+        let from_token_acct = getOrCreateAssociatedTokenAccount(provider.connection, deposit_account, mint.publicKey, deposit_auth.publicKey, false);
+        let from_token_acct_wallet = (await from_token_acct).address;
+
+        let withdraw_spl_tx = await program.methods.withdrawSpl(new anchor.BN(25e6)).accounts({
+            depositAccount: deposit_account.publicKey,
+            pdaAuth: pda_auth,
+            depositAuth: deposit_auth.publicKey,
+            toTokenAcct: to_token_acct,
+            fromTokenAcct: from_token_acct_wallet,
+            tokenMint: mint.publicKey,
+            tokenProgram: TOKEN_PROGRAM_ID,
+            associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+            systemProgram: anchor.web3.SystemProgram.programId,
+        }).signers([deposit_auth]).rpc();
+
+        console.log(withdraw_spl_tx);
+    });
+
+    it("mints 1 NFT & sends it", async () => {
+        //creates a metadata
+        const { uri } = await metaplex.nfts().uploadMetadata({
+            name: "WBA Diploma #441",
+            description: "The NFT represents the skillset of the WBA graduate",
+            image: "https://arweave.net/123",
+            github: "https://github.com/ilyxabatko",
+            skillset: "Rust, Native Solana, Anchor, TypeScript, Mocha.js, React.js, Next.js"
+        });
+
+        // mints 1 nft
+        let nft_mint = await metaplex.nfts().create({
+            name: "WBA Diploma #441",
+            symbol: "WBAD",
+            uri: uri,
+            sellerFeeBasisPoints: 300, // 3%
+            isMutable: false,
+            maxSupply: toBigNumber(1)
+        });
+
+        // gets and prints mint's info
+        const nft_mint_info = await getMint(provider.connection, nft_mint.mintAddress, null, TOKEN_PROGRAM_ID);
+        console.log("NFT MINT INFO: ", nft_mint_info);
+
+        // Derives the metadata and master edition addresses 
+
+        const metadata_address = (anchor.web3.PublicKey.findProgramAddressSync([
+                Buffer.from("metadata"),
+                TOKEN_METADATA_PROGRAM_ID.publicKey.toBuffer(),
+                nft_mint.mintAddress.toBuffer()
+            ],
+            TOKEN_METADATA_PROGRAM_ID.publicKey
+        ))[0];
+        console.log("Metadata initialized: ", metadata_address);
+
+        const master_edition_address = (anchor.web3.PublicKey.findProgramAddressSync([
+                Buffer.from("metadata"),
+                TOKEN_METADATA_PROGRAM_ID.publicKey.toBuffer(),
+                nft_mint.mintAddress.toBuffer(),
+                Buffer.from("edition")
+            ],
+            TOKEN_METADATA_PROGRAM_ID.publicKey
+        ))[0];
+        console.log("Master edition initialized: ", master_edition_address);
+
+        let to_token_acct = getAssociatedTokenAddressSync(nft_mint.mintAddress, pda_auth, true, TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID);
+
+        // sends an NFT to an ATA 
+        let deposit_nft_tx = await program.methods.depositSpl(new anchor.BN(1)).accounts({
+            depositAccount: deposit_account.publicKey,
+            pdaAuth: pda_auth,
+            depositAuth: deposit_auth.publicKey,
+            toTokenAcct: to_token_acct,
+            fromTokenAcct: deposit_account.publicKey,
+            tokenMint: mint.publicKey,
+            tokenProgram: TOKEN_PROGRAM_ID,
+            associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+            systemProgram: anchor.web3.SystemProgram.programId,
+        }).signers([deposit_auth]).rpc();
+
+        console.log(deposit_nft_tx);
+
+    });
+
 });
